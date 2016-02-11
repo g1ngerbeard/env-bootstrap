8ab6d6cb06a4c9eb3caffbd086a1f03f0a263df0
gingerbeard <kulishovt@gmail.com>
2016-02-11 21:39:33 +0100

    moved aliases to separate script

--- env-bootstap.sh
+++ env-bootstap.sh
@@ -1,10 +1,3 @@
 #!/usr/bin/env bash
-echo '
-alias idea-keyboard="ibus-daemon -rd"
-alias mcinstall="mvn clean install -DskipTests"
-alias mci2dock="mvn clean install -DskipTests && mvn docker:build"
-alias docker-clean="docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)"
-alias pbcopy="xclip -selection clipboard"
-alias pbpaste="xclip -selection clipboard -o"
-' >> ~/.bashrc
 
+cat aliases.sh >> ~/.bashrc
